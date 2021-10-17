c706b5aeb3b10682c789730ad37a706bc1d1832a
Neville <nevillemathew2014@gmail.com>
2021-10-17 23:54:52 +0100

    Changed variable names to PEP standard, added book returns function

--- menu.py
+++ menu.py
@@ -22,25 +22,41 @@ def recommendations() -> None:
 def checkIn() -> None:
     global BOOK_IMG
     BOOK_IMG = ImageTk.PhotoImage(Image.open('book.jpg'))
-    book_Img_label = Label(BOOK_CHECKIN, image=BOOK_IMG)
-    book_Img_label.pack(side=TOP)
-    member_Id = Label(BOOK_CHECKIN, text='Member ID',
+    book_img_label = Label(BOOK_CHECKIN, image=BOOK_IMG)
+    book_img_label.pack(side=TOP)
+    member_id = Label(BOOK_CHECKIN, text='Member ID',
                       font=('Arial', 18), bg='#FEEAE6')
-    member_Id.pack(padx=400, pady=40, side=TOP)
-    E1 = Entry(BOOK_CHECKIN, bd=2, bg='Light Blue',
-               justify=CENTER, font=('Arial', 13))
-    E1.pack(side=TOP)
-    book_Id = Label(BOOK_CHECKIN, text='Book ID',
-                      font=('Arial', 18), bg='#FEEAE6')
-    book_Id.pack(padx=400, pady=40, side=TOP)
-    E2 = Entry(BOOK_CHECKIN, bd=2, bg='Light Blue',
-               justify=CENTER, font=('Arial', 13))
-    E2.pack(side=TOP)
-    B = Button(BOOK_CHECKIN, text ="Book Check In", bg= 'Light Blue', font=('Arial', 13))
-    B.pack(pady=20, side=TOP)
+    member_id.pack(padx=400, pady=40, side=TOP)
+    member_id_entry = Entry(BOOK_CHECKIN, bd=2, bg='Light Blue',
+                            justify=CENTER, font=('Arial', 13))
+    member_id_entry.pack(side=TOP)
+    book_id = Label(BOOK_CHECKIN, text='Book ID',
+                    font=('Arial', 18), bg='#FEEAE6')
+    book_id.pack(padx=400, pady=40, side=TOP)
+    book_id_entry = Entry(BOOK_CHECKIN, bd=2, bg='Light Blue',
+                          justify=CENTER, font=('Arial', 13))
+    book_id_entry.pack(side=TOP)
+    book_checkIn_button = Button(BOOK_CHECKIN, text="Book Check In",
+                                 bg='Light Blue', font=('Arial', 13))
+    book_checkIn_button.pack(pady=20, side=TOP)
+
+
+def returns():
+    global BOOK_IMG1
+    BOOK_IMG1 = ImageTk.PhotoImage(Image.open('book return.jpg'))
+    book_img_label1 = Label(BOOK_RETURN, image=BOOK_IMG1)
+    book_img_label1.pack(side=TOP)
+    book_id1 = Label(BOOK_RETURN, text='Book ID',
+                     font=('Arial', 18), bg='#FEEAE6')
+    book_id1.pack(padx=400, pady=40, side=TOP)
+    book_id_entry1 = Entry(BOOK_RETURN, bd=2, bg='Light Blue',
+                           justify=CENTER, font=('Arial', 13))
+    book_id_entry1.pack(side=TOP)
+    book_return_button = Button(BOOK_RETURN, text="Book Return",
+                                bg='Light Blue', font=('Arial', 13))
+    book_return_button.pack(pady=20, side=TOP)
 
 #####MAIN FUNCTIONS######
-
 
 WINDOW = Tk()
 WINDOW.title("Library Management System - ADMIN")
@@ -62,4 +78,5 @@ TAB_CONTROL.pack(expand=1, fill="both")
 
 recommendations()
 checkIn()
+returns()
 WINDOW.mainloop()
